0ba64de64b7dec8433cd7803e61c54ede76b65b0
mateusz <mateuszlewandowski016@gmail.com>
2024-02-20 17:16:58 +0100

    new

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+#zadanie1
+print('zadanie 1')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(hex(2024))
+
+x = -32
+print(abs(x))
+
+data = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+print(max(data))
+
+print(pow(x, 2))
+
+print(ord('a'))
+
+#zadanie2
+print('zadanie 2')
+from zadanie2 import *
+welcome()
+
+#zadanie3
+print('zadanie 3')
+def fun1():
+    x = 12
+    print("wartość x wewnątrz funkcji: ",x)
+
+fun1()
+print("wartość x poza funkcją: ",x)
+
+#zadanie4
+print('zadanie4')
+def funz4_a():
+    return 10
+def funz4_b(funz4_a):
+    print("Funkcja funz4_a zwaraca za pomocą return wartość: ", funz4_a())
+funz4_b(funz4_a)
+
+#zadanie5
+print('zadanie5')
+numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+def isEven(list):
+    for i in list:
+        if (i % 2 == 0):
+            print(i)
+isEven(numbers)
+
+x = lambda a, b : a * b
+print('Pole kwadratu:', x(5, 6))
+
+#zadanie6
+print('zadanie 6')
+def start_a(name):
+    return list(filter(lambda x: x[0] == 'A', name))
+def pow_list(data):
+    return list(map(lambda x: x*x, data))
+
+print(start_a(['Adam', 'Mateusz', 'Adrian']))
+print(pow_list([1, 2, 3, 4, 5]))
+
+#zadanie7
+print('zadanie 7')
+square = lambda number: number * number
+plusFive = lambda number: number + 5
+addNumbers = lambda a,b: a + b
+
+def apply(data):
+    data = map(square, data)
+    data = map(plusFive, data)
+    return list(data)
+
+def applyToList(functions, args):
+    for f, n in zip(functions, args):
+        print(f(*n) if isinstance(n, tuple) else f(v))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#zadanie8
